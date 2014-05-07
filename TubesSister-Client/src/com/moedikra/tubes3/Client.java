470ea50e8dba7a6203dad11d1c28384c993ed613
M Dikra Prasetya <dikraprasetya@yahoo.co.id>
2014-05-07 22:49:22 +0700

    submitted for part 1

--- TubesSister-Client/src/com/moedikra/tubes3/Client.java
+++ TubesSister-Client/src/com/moedikra/tubes3/Client.java
@@ -3,8 +3,8 @@ import java.io.*;
 import java.net.*;
 
 public class Client {
-    public static void main(String[] args) throws IOException {
-        
+    public static void main(String[] args) throws IOException {  
+    	    	
         if (args.length != 2) {
             System.err.println(
                 "Usage: java EchoClient <host name> <port number>");
